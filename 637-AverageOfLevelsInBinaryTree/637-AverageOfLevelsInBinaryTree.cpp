c0c05277513ea69255e17b1938f735a996b616dd
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-05-10 23:50:28 +0530

    Create 637-AverageOfLevelsInBinaryTree

---
+++ 637-AverageOfLevelsInBinaryTree/637-AverageOfLevelsInBinaryTree.cpp
@@ -0,0 +1,35 @@
+// Last updated: 5/10/2026, 11:50:29 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        if(!root) return {};
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<double>result;
+        while(!q.empty()){
+            int size = q.size();
+            double sum = 0;
+            int count = 0;
+            for(int i=0; i<size; i++){
+                TreeNode* front = q.front(); q.pop();
+                sum += front->val;
+                count++;
+                if(front->left)q.push(front->left);
+                if(front->right)q.push(front->right);
+            }
+            result.push_back(sum/count);
+        }
+        return result;
+    }
+};
