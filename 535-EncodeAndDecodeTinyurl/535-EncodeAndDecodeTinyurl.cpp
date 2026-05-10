c3401728f88f20a2e0e58342736a7c23df57c981
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-05-10 23:50:47 +0530

    Create 535-EncodeAndDecodeTinyurl

---
+++ 535-EncodeAndDecodeTinyurl/535-EncodeAndDecodeTinyurl.cpp
@@ -0,0 +1,10 @@
+// Last updated: 5/10/2026, 11:50:48 PM
+class Solution {
+public:
+    string encode(string longUrl) {
+        return longUrl;
+    }
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
