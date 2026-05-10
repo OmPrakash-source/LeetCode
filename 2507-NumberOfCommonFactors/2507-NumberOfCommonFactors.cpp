bbab764f5373e3a7c8fc83162b45bf26f384309c
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-05-10 23:45:40 +0530

    Create 2507-NumberOfCommonFactors

---
+++ 2507-NumberOfCommonFactors/2507-NumberOfCommonFactors.cpp
@@ -0,0 +1,12 @@
+// Last updated: 5/10/2026, 11:45:41 PM
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count = 0;
+        for(int i=1; i<= min(a,b); i++)
+            if(a%i==0 && b%i == 0) 
+                count++;
+        
+        return count;
+    }
+};
