2c33f2163a13d4a6a77f52e24d1a2a85df89d22a
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-06-28 15:58:52 +0530

    Time: 0 ms (100%), Space: 8 MB (4.43%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int bitwiseComplement(int num) {
+        if (num == 0) return 1;
+        string st = "";
+
+        while(num > 0){
+            st += (num % 2) + '0';
+            num /= 2;
+        }
+        reverse(st.begin(), st.end());
+        
+        for(int i=0; i<st.size(); i++){
+            char ch = st[i];
+
+            if(ch == '0'){
+                st[i] = '1';
+            }else if(ch == '1'){
+                st[i] = '0';
+            }
+        }
+        cout<<st<< " ";
+        int result = 0;
+        int j = st.size() - 1;
+        
+        for (int i = 0; i < st.size(); i++) {
+            result += (st[i] - '0') * (1 << j);
+            j--;
+        }
+
+        return result;
+    }
+};
