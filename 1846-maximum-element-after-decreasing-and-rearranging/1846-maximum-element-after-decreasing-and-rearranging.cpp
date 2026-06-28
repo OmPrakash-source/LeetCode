90e1f954d31102838847f8812e26f8c58c2abbb3
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-06-28 20:33:13 +0530

    Time: 9 ms (54.24%), Space: 55.2 MB (36.72%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -8,10 +8,9 @@ public:
         int maxi = INT_MIN;
 
         for(int i=1; i<nums.size(); i++){
-            if(abs(nums[i-1] - nums[i]) > 1){
+            if(nums[i] - nums[i-1] > 1){
                 nums[i] = min(nums[i], nums[i-1]+1);
             }
-            cout<<nums[i]<<" ";
             maxi = max(nums[i], maxi);
         }
         return maxi;
