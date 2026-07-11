723aa8f893e1a6535acbff3982e1978ee4d22363
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-07-12 00:13:12 +0530

    Time: 18 ms (15.27%), Space: 14 MB (43.42%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -4,19 +4,39 @@ public:
         int n = nums.size();
         int diff = INT_MAX;
         int result = 0;
+        // for(int i=0; i<n-2; i++){
+        //     for(int j=i+1; j<n-1; j++){
+        //         int sum = nums[i] + nums[j];
+        //         for(int k=j+1; k<n; k++){
+        //             sum+=nums[k];
+        //             if(abs(target - sum) < diff){
+        //                 result = sum;
+        //                 diff = min(abs(target - sum), diff);
+        //             }
+        //             sum -= nums[k];
+        //         }
+        //     }
+        // }
+        // return result;
+
+        sort(nums.begin(), nums.end());
         for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
-                int sum = nums[i] + nums[j];
-                for(int k=j+1; k<n; k++){
-                    sum+=nums[k];
-                    if(abs(target - sum) < diff){
+            int j = i+1, k = n-1;
+            
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(abs(target - sum) < diff){
                         result = sum;
-                        diff = min(abs(target - sum), diff);
-                    }
-                    sum -= nums[k];
+                        diff = abs(target - sum);
+                }
+                if(sum > target){
+                    k--;
+                }
+                else{
+                    j++;
                 }
             }
         }
-        return result;
+    return result;
     }
 };
