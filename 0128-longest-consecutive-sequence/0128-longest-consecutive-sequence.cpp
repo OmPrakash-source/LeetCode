7b9c6100d5665f201113b46ea8ae7cca7efb5ddc
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-07-15 17:16:24 +0530

    Time: 0 ms (100%), Space: 60.2 MB (92.74%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int longest = 0;
+        int longest = 1;
         
-        unordered_set<int> m(nums.begin(), nums.end());
+        // unordered_set<int> m(nums.begin(), nums.end());
         if(nums.size() == 0) return 0;
 
-        for (auto i : m) {
-            if (m.find(i - 1) == m.end()) { 
-                int count = 1;
-                while (m.find(i + count) != m.end()) {
-                    count++;
-                }
-                longest = max(longest, count);
+        // for (auto i : m) {
+        //     if (m.find(i - 1) == m.end()) { 
+        //         int count = 1;
+        //         while (m.find(i + count) != m.end()) {
+        //             count++;
+        //         }
+        //         longest = max(longest, count);
+        //     }
+        // }
+
+        sort(nums.begin(), nums.end());
+        int count = 1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i-1] == nums[i]) continue;
+            else if((nums[i-1] + 1) != nums[i])count = 1;
+            else{
+                count++;
+                cout<<nums[i]<<" "<<nums[i-1]<<endl;
             }
+
+            longest = max(longest, count);
         }
         return longest;
     }
