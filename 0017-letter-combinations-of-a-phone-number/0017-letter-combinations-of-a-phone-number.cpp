8019dba43c0875972ff1be524a874797ec095750
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-07-29 00:30:50 +0530

    Time: 0 ms (100%), Space: 9.1 MB (90.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    void func(int idx, string op, vector<string>&ans, vector<string>&st, string &dig){
+    // void func(int idx, string &op, vector<string>&ans, vector<string>&st, string &dig){
+    //     if(idx >= dig.size()){
+    //         ans.push_back(op);
+    //         return;
+    //     }
+
+    //     int num = dig[idx] - '0';
+    //     string val = st[num];
+    //     for(int i=0; i<val.size(); i++){
+    //         op.push_back(val[i]);
+    //         func(idx+1, op, ans, st, dig);
+    //         op.pop_back();
+    //     }
+    // }
+    vector<string> letterCombinations(string digit) {
+        // vector<string>ans;
+        // if(digit.size() == 0)return ans;
+        // vector<string>st = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        // func(0, "", ans, st, digit);
+        // return ans;
+        vector<string>result;
+        string ans;
+        vector<string>key = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        func(0, ans, key, result, digit);
+        return result;
+    }
+
+    void func(int idx, string &ans, vector<string>&key, vector<string>&rlt, string &dig){
         if(idx >= dig.size()){
-            ans.push_back(op);
+            rlt.push_back(ans);
             return;
         }
-
-        int num = dig[idx] - '0';
-        string val = st[num];
+        string val = key[dig[idx]-'0'];
         for(int i=0; i<val.size(); i++){
-            op.push_back(val[i]);
-            func(idx+1, op, ans, st, dig);
-            op.pop_back();
+            ans.push_back(val[i]);
+            func(idx+1, ans, key, rlt, dig);
+            ans.pop_back();
         }
-    }
-    vector<string> letterCombinations(string digit) {
-        vector<string>ans;
-        if(digit.size() == 0)return ans;
-        vector<string>st = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        func(0, "", ans, st, digit);
-        return ans;
     }
 };
