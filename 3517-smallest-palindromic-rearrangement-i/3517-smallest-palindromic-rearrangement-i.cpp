d57872d5deebf6980a06e2e7a93ae72bc901b823
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-07-28 22:26:08 +0530

    Time: 61 ms (45.49%), Space: 54 MB (98.46%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        sort(s.begin(), s.end());
+        // sort(s.begin(), s.end());
 
-        string left = "";
-        string mid = "";
+        // string left = "";
+        // string mid = "";
 
-        int i = 0;
-        while (i < s.size()) {
-            int j = i;
-            while (j < s.size() && s[j] == s[i])
-                j++;
+        // int i = 0;
+        // while (i < s.size()) {
+        //     int j = i;
+        //     while (j < s.size() && s[j] == s[i])
+        //         j++;
+        //     int cnt = j - i;
+        //     left += string(cnt / 2, s[i]);
+        //     if (cnt % 2)
+        //         mid = s[i];
+        //     i = j;
+        // }
 
-            int cnt = j - i;
+        // string right = left;
+        // reverse(right.begin(), right.end());
 
-            left += string(cnt / 2, s[i]);
+        // return left + mid + right;
 
-            if (cnt % 2)
-                mid = s[i];
-
-            i = j;
+        int n=s.length();
+        sort(s.begin(),s.begin() + n/2);
+        for(int i=0;i<n/2;i++){
+            s[n-1-i]=s[i];
         }
-
-        string right = left;
-        reverse(right.begin(), right.end());
-
-        return left + mid + right;
+        return s;
     }
 };
