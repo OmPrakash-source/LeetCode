71dbf68de5372ff098c99aa267267309c834bfa6
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-07-30 22:37:22 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int sum = 0;
-        int multi = 1;
-        int cnt = 0;
-        for(char ch : word){
-            if(sum != 0 && sum % 8 == 0){
-                sum = 0;
-                multi += 1;
-            }
-            sum++;
-            cnt += multi;
-        }
-        return cnt;
+
+        int n = word.size();
+        int full = n / 8;
+        int rem = n % 8;
+
+        return 8 * full * (full + 1) / 2 + rem * (full + 1);
+
+
+        // int sum = 0;
+        // int multi = 1;
+        // int cnt = 0;
+        // for(char ch : word){
+        //     if(sum != 0 && sum % 8 == 0){
+        //         sum = 0;
+        //         multi += 1;
+        //     }
+        //     sum++;
+        //     cnt += multi;
+        // }
+        // return cnt;
     }
 };
