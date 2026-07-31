49c09b516056c9819ecaa332d6cf19e2f8a212bf
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-07-31 23:14:45 +0530

    Time: 7 ms (69.76%), Space: 25.3 MB (94.44%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char,int>mpp;
-        for(char ch : word) mpp[ch]++;
+        // unordered_map<char,int>mpp;
+        // for(char ch : word) mpp[ch]++;
 
-        vector<int>freq;
-        
-        for(auto it : mpp) freq.push_back(it.second);
+        // vector<int>freq;
+        // for(auto it : mpp) freq.push_back(it.second);
+        // sort(freq.begin(),freq.end(), greater<int>());
+        // int ans = 0;
+        // for(int i=0; i<freq.size(); i++){
+        //     ans += freq[i] * (i/8 + 1);
+        // }
+        // return ans;
+
+
+        // int ans = 0;
+        vector<int>freq(26, 0);
+        for(char ch : word) freq[ch - 'a']++;
         sort(freq.begin(),freq.end(), greater<int>());
         int ans = 0;
         for(int i=0; i<freq.size(); i++){
+            if(freq[i] == 0) continue;
             ans += freq[i] * (i/8 + 1);
         }
         return ans;
