8204a60d90eee7f5871a38ce7be1ba9c8bd9bb45
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-08-03 22:46:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (82.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-
-    void func(int idx, vector<int>&temp, vector<int>& nums, vector<vector<int>>&ans){
-        if(idx >= nums.size()){
+    void solve(vector<int>& nums,vector<vector<int>>&ans, vector<int>&temp,int n){
+        if(n == nums.size()) {
             ans.push_back(temp);
             return;
         }
-        func(idx+1, temp, nums, ans);
-        temp.push_back(nums[idx]);
-        func(idx+1, temp, nums, ans);
-        temp.pop_back();
+        
+        temp.push_back(nums[n]);
+        solve(nums, ans, temp, n + 1);
+        temp.pop_back(); 
+
+        solve(nums,ans,temp,n+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>temp;
-        func(0, temp, nums, ans);
+        solve(nums, ans,temp, 0);
+        // sort(ans.begin(),ans.end());
         return ans;
     }
 };
