609b9949d8d7d28dc674daabbdf1542e7b8bccd5
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-08-17 00:10:20 +0530

    Time: 135 ms (63.01%), Space: 134.1 MB (57.53%) - LeetHub

---
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        unordered_map<string, long long>mpp;
+        for(int i=0; i<creators.size(); i++){
+            mpp[creators[i]] += views[i];
+        }
+        long long maxi = 0;
+        for(auto &it : mpp) maxi = max(maxi, it.second);
+        unordered_map<string, pair<long long,string>> best;
+
+        vector<vector<string>>result;
+        for(int i=0; i<creators.size(); i++){
+           if(best.find(creators[i]) == best.end() || views[i] > best[creators[i]].first || (views[i] == best[creators[i]].first && ids[i] < best[creators[i]].second)){
+                best[creators[i]] = {views[i], ids[i]};
+            }
+        }
+        for(auto &it : best){
+            if(mpp[it.first] == maxi){
+                result.push_back({it.first, it.second.second});
+            }
+        }
+        return result;
+    }
+};
