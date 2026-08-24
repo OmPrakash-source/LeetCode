51dbaf520e53321a0d1f1e6b1240dc4a1df5a2cd
OMPRAKASH <182613754+OmPrakash-source@users.noreply.github.com>
2026-08-24 15:12:45 +0530

    Time: 3 ms (18.32%), Space: 40.8 MB (80.22%) - LeetHub

---
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int scoreDifference(vector<int>& nums) {
+        bool flag = true;
+        int p1 = 0, p2 = 0;
+        int sswap = 0;
+        for(int i=0; i<nums.size(); i++){
+            sswap++;
+            if(nums[i] % 2 != 0) flag = !flag;
+            if(sswap % 6 == 0) flag = !flag;
+
+            if(flag) p1 += nums[i];
+            else p2 += nums[i];
+        }
+        return p1-p2;
+    }
+};
